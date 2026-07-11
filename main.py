31574d7c9edc33f75ead4fb27a203321d33f1ac9
Jemima-Math <jemimanourin04@gmail.com>
2026-07-11 11:08:38 +0530

    Day 1 practice

--- main.py
+++ main.py
@@ -1,1 +1,40 @@
-print("Hello from Github!")
+print("Hello world")
+print(5)
+print(15*13)
+
+# use this for comment
+# use \n for line change
+print("My name is Jemima\n and i am a student of JU.")
+
+print("Hey",6 ,7,sep="~",end="009")
+
+a = complex(8,2)
+b = True
+c = "Mathematics"
+d = None
+print(a)
+print(b)
+a1 = 9
+print(a+a1)
+print("The Type of a is",type(a))
+print("The Type of b is",type(b))
+print("The Type of c is",type(c))
+
+list1 = [8,2.3,[-4, 5],["apple","Banana"]]
+print(list1)
+tuple1 = (("parrot","Sparrow"),("Lion","Tiger"))
+print(tuple1)
+dict1 = {"name":"Jemima","age":21,"canVote":True}
+print(dict1)
+
+a = "4"
+b = "5"
+print(int(a)+int(b))
+
+#Implicit Typecasting
+a = 3
+b = 4.9
+print(a+b)
+
+#Explicit Typecasting
+print(int(a+b))
