cdb39a0d5cd937f5ed5c64e9faa0c946a2eb25c1
Jemima-Math <jemimanourin04@gmail.com>
2026-08-07 21:41:46 +0530

    day8practice

--- day8.py
+++ day8.py
@@ -43,3 +43,45 @@ lst= [i*i for i in range(10)]
 print(lst)
 lst= [i*i for i in range(10) if i%2 == 0]
 print(lst)
+
+l = [31, 45, 5, 3, 9]
+l.append(7) # append will add 
+print(l)
+# print(l.append(7).sort())
+
+l = [31, 45, 5, 3, 9]
+l.sort()  # sort in assending order 
+print(l)
+
+l = [31, 45, 5, 3, 9]
+l.sort(reverse=True)  # sort in desending order 
+print(l)
+
+l = [31, 45, 5, 3, 9]
+l.reverse()  # just reverse the list
+print(l)
+
+l = [31, 45, 5, 1, 3, 9]
+print(l.index(1))  # posion of 1
+
+l = [31, 1, 45, 5, 1, 3, 9]
+print(l.count(1))  # how many 1 has
+
+l = [31, 45, 5, 1, 3, 9]
+m = l.copy()
+print(l)
+
+l = [31, 45, 5, 38, 1, 3, 9]
+l.insert(1 , 899)  # 1 is denoted the position 899 will take in list
+print(l)
+
+l = [31, 45, 5, 1, 3, 9]
+m =[900, 1000, 1111]
+l.extend(m)  # if we want to extend l,m will add in last
+print(l)
+# another way to extend:
+
+l = [31, 45, 5, 1, 3, 9]
+m =[900, 1000, 1111]
+k = l + m
+print(k)
